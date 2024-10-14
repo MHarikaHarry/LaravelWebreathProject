a23c4060a57fed858a74c7e3bfca9914e31e8125
MHarikaHarry <122019458+MHarikaHarry@users.noreply.github.com>
2024-10-14 22:28:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,7 @@
 provider "aws" {
   aws-region: ${{ secrets.AWS_DEFAULT_REGION }}
 }
-- name: Set up AWS credentials
-  uses: aws-actions/configure-aws-credentials@v1
-  with:
-       aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-       aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-       
-
+    
 resource "aws_instance" "laravel_instance" {
   ami           = "ami-0866a3c8686eaeeba"  # Ubuntu AMI ID
   instance_type = "t2.micro"
